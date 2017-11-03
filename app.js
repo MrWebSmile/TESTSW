7f80ac5c9d0443548da4fe689b5664d017042850
MrWebSmile <MrWebSmile@users.noreply.github.com>
2017-11-03 09:09:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/TESTSW/sw.js', { scope: '/TESTSW/' })
-    .then(function(reg) {
-      // suivre l'état de l'enregistrement du Service Worker : `installing`, `waiting`, `active`
-    });
-}
+alert("test");
